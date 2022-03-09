d7270cfd3904a1b33a19a79068a7b5b68a917a40
Mattia Bardino <Mattia.errol@gmail.com>
2022-03-09 23:47:45 +0100

    .

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php
+   $paragrafo = "il mio primo nuovo progetto con php";
+
+   $censura = "***";
+
+?>
+
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+
+    <h1>Ciao questo è <?php echo$paragrafo ?>!</h1>
+
+    <h4>La mia stringa è lunga: "<?php echo strlen($paragrafo)?>" caratteri.</h4>
+
+    <?php echo str_replace('nuovo', $censura, $paragrafo) ?>
+
+
+
+    
+</head>
+<body>
+    
+</body>
+</html>
